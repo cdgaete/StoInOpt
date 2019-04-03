b66bf359781f2006d906fbdc2a67d3f150b00885
unknown <miguelfastudillo@gmail.com>
2019-04-03 21:40:22 +0200

    grafter is born

---
+++ docs/index.rst
@@ -0,0 +1,20 @@
+.. grafter documentation master file, created by
+   sphinx-quickstart on Wed Apr  3 21:19:33 2019.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to grafter's documentation!
+===================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ if root_dir:
 
 
 # TODO
-# Change every instance of `your_library_name` to the name of your library.
 # Change `author`
 # Change `author_email`
 # Change `url`
@@ -16,7 +15,7 @@ if root_dir:
 
 
 # Probably should be changed, __init__.py is no longer required for Python 3
-for dirpath, dirnames, filenames in os.walk('your_library_name'):
+for dirpath, dirnames, filenames in os.walk('grafter'):
     # Ignore dirnames that start with '.'
     if '__init__.py' in filenames:
         pkg = dirpath.replace(os.path.sep, '.')
@@ -34,26 +33,26 @@ def package_files(directory):
 
 
 setup(
-    name='your_library_name',
+    name='grafter',
     version="0.1",
     packages=packages,
-    author="your_name_here",
-    author_email="your_email_here",
+    author="BONSAI team",
+    author_email="info@bonsai.uno",
     license=open('LICENSE').read(),
     # Only if you have non-python data (CSV, etc.). Might need to change the directory name as well.
-    # package_data={'your_name_here': package_files(os.path.join('your_library_name', 'data'))},
+    # package_data={'BONSAI team': package_files(os.path.join('grafter', 'data'))},
     entry_points = {
         'console_scripts': [
-            'your_library_name-cli = your_library_name.bin.rename_me_cli:main',
+            'grafter-cli = grafter.bin.rename_me_cli:main',
         ]
     },
     install_requires=[
         'appdirs',
         'docopt',
     ],
-    url="your_url_here",
+    url="https://github.com/BONSAMURAIS/grafter",
     long_description=open('README.md').read(),
-    description='your_name_here',
+    description='create correspondance tables with semantic meaning',
     classifiers=[
         'Intended Audience :: End Users/Desktop',
         'Intended Audience :: Developers',
