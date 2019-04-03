f5920ac375cbf1d6460a3275365d0d4f08512ea2
Chris Mutel <cmutel@gmail.com>
2019-04-03 22:42:09 +0200

    Revert inadvertant library start
    This reverts commit b66bf359781f2006d906fbdc2a67d3f150b00885.

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ if root_dir:
 
 
 # TODO
+# Change every instance of `your_library_name` to the name of your library.
 # Change `author`
 # Change `author_email`
 # Change `url`
@@ -15,7 +16,7 @@ if root_dir:
 
 
 # Probably should be changed, __init__.py is no longer required for Python 3
-for dirpath, dirnames, filenames in os.walk('grafter'):
+for dirpath, dirnames, filenames in os.walk('your_library_name'):
     # Ignore dirnames that start with '.'
     if '__init__.py' in filenames:
         pkg = dirpath.replace(os.path.sep, '.')
@@ -33,26 +34,26 @@ def package_files(directory):
 
 
 setup(
-    name='grafter',
+    name='your_library_name',
     version="0.1",
     packages=packages,
-    author="BONSAI team",
-    author_email="info@bonsai.uno",
+    author="your_name_here",
+    author_email="your_email_here",
     license=open('LICENSE').read(),
     # Only if you have non-python data (CSV, etc.). Might need to change the directory name as well.
-    # package_data={'BONSAI team': package_files(os.path.join('grafter', 'data'))},
+    # package_data={'your_name_here': package_files(os.path.join('your_library_name', 'data'))},
     entry_points = {
         'console_scripts': [
-            'grafter-cli = grafter.bin.rename_me_cli:main',
+            'your_library_name-cli = your_library_name.bin.rename_me_cli:main',
         ]
     },
     install_requires=[
         'appdirs',
         'docopt',
     ],
-    url="https://github.com/BONSAMURAIS/grafter",
+    url="your_url_here",
     long_description=open('README.md').read(),
-    description='create correspondance tables with semantic meaning',
+    description='your_name_here',
     classifiers=[
         'Intended Audience :: End Users/Desktop',
         'Intended Audience :: Developers',
